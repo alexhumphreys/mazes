565e1cb29dc4165b83cc64e98763e782c4a48e3c
Alex Humphreys <humphreys.a@gmail.com>
2015-02-11 23:41:12 +0100

    * refactored binarytree algorithm

--- src/mazes1/BinaryTreeMaze.scala
+++ src/mazes1/BinaryTreeMaze.scala
@@ -1,35 +1,18 @@
 package mazes1
 
 object BinaryTreeMaze {
+  
   def on(grid: Grid) : Grid = {
-    grid.eachCell foreach { cell =>
-      
-      println(cell)
-      
-      val neighbors = new Array[Option[Cell]](2)
-      
-      neighbors(0) = cell.north
-      neighbors(1) = cell.east
-      
-      println(neighbors)
-      
-      val r = scala.util.Random
-      val index = r.nextInt(neighbors.length)
-      val neighbor = neighbors(index)
-      
-      println("index", index)
-      println("neighbor defined", neighbor.isDefined)
-      
-      if (neighbor.isDefined) {
-        cell.link(neighbor.get)
-      } else {
-        val newIndex = if (index == 0) 1 else 0;
-        if (neighbors(newIndex).isDefined) cell.link(neighbors(newIndex).get)
-      }
-        
-      println("links", cell.links)
+    grid.eachCell foreach { cell =>     
+      val neighbors = List(cell.north, cell.east).flatten    
+      if (neighbors.length > 0) cell.link(getRandomCell(neighbors))
     }
-    
     grid
   }
+  
+  def getRandomCell(list: List[Cell]) : Cell = {
+    if (list.length == 1) return list(0) 
+    val random = new scala.util.Random
+    list(random.nextInt(list.length))
+  }  
 }
